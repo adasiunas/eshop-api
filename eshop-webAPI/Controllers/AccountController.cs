8fa4574129c404742e71f0cc326f6dc5844cb68f
Marius Adašiūnas <marius.adasiunas@gmail.com>
2018-03-03 21:32:54 +0200

    added swagger

---
+++ eshop-webAPI/Controllers/AccountController.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace eshop_webAPI.Controllers
+{
+    [Route("api/account")]
+    public class AccountController : Controller
+    {
+        
+        [HttpGet("profile")]
+        public IActionResult Profile()
+        {
+            return Ok();
+        }
+        [HttpPost("register")]
+        public IActionResult Register()
+        {
+            return Ok();
+        }
+        
+        [HttpPost("login")]
+        public IActionResult Login()
+        {
+            return Ok();
+        }
+        
+        [HttpPost("logout")]
+        public IActionResult Logout()
+        {
+            return Ok();
+        }
+        
+        [HttpPost("varify/email")]
+        public IActionResult VarifyEmail()
+        {
+            return Ok();
+        }
+        
+        [HttpPost("reset/password")]
+        public IActionResult ResetPassword()
+        {
+            return Ok();
+        }
+        
+        [HttpPost("remember")]
+        public IActionResult RememberPassword()
+        {
+            return Ok();
+        }
+    }
+}
--- eshop-webAPI/Startup.cs
+++ eshop-webAPI/Startup.cs
@@ -8,6 +8,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using Swashbuckle.AspNetCore.Swagger;
 
 namespace eshop_webAPI
 {
@@ -24,6 +25,10 @@ namespace eshop_webAPI
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc();
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new Info { Title = "eshop-api", Version = "v1" });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -32,6 +37,11 @@ namespace eshop_webAPI
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI(c =>
+                {
+                    c.SwaggerEndpoint("/swagger/v1/swagger.json", "eshop-api V1");
+                });
             }
 
             app.UseMvc();
