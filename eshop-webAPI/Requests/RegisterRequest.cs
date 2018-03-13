6cd88dbfb0429e9d41fa6e13438a496de3ec5050
Marius Adašiūnas <marius.adasiunas@gmail.com>
2018-03-13 22:57:36 +0200

    implementet Fluent validator for api

---
+++ eshop-webAPI/Requests/RegisterRequest.cs
@@ -0,0 +1,9 @@
+﻿namespace eshopAPI.Requests
+{
+    public class RegisterRequest
+    {
+        public string Username { get; set; } // Email
+        public string Password { get; set; }
+        public string ConfirmPassword { get; set; }
+    }
+}
