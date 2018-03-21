a590b17b80b3daf48588eb6ee7466618af532269
Greta Mameniskyte <gretuka27@gmail.com>
2018-03-21 09:14:01 +0100

    Add address table for user delivery addresses

---
+++ eshop-webAPI/Migrations/20180321075736_DeliveryAddress.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace eshopAPI.Migrations
+{
+    public partial class DeliveryAddress : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Address",
+                columns: table => new
+                {
+                    ID = table.Column<long>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    City = table.Column<string>(nullable: false),
+                    Country = table.Column<string>(nullable: false),
+                    Name = table.Column<string>(nullable: false),
+                    Postcode = table.Column<string>(nullable: false),
+                    ShopUserId = table.Column<string>(nullable: true),
+                    Street = table.Column<string>(nullable: false),
+                    Surname = table.Column<string>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Address", x => x.ID);
+                    table.ForeignKey(
+                        name: "FK_Address_AspNetUsers_ShopUserId",
+                        column: x => x.ShopUserId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Address_ShopUserId",
+                table: "Address",
+                column: "ShopUserId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Address");
+        }
+    }
+}
