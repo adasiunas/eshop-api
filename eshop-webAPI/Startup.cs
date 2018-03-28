c3e7ec319d6d3cc3c3b0a6c64598252fe90b66b4
Saulius Skliutas <saulius.skliutas@gmail.com>
2018-03-28 23:13:15 +0300

    Missing dot

--- eshop-webAPI/Startup.cs
+++ eshop-webAPI/Startup.cs
@@ -41,7 +41,7 @@ namespace eshopAPI
             services.ConfigureApplicationCookie(options =>
             {
                 options.Cookie.Name = "SecCookie";
-                options.Cookie.Domain = "eshop-qa-web.azurewebsites.net";
+                options.Cookie.Domain = ".eshop-qa-web.azurewebsites.net";
                 options.Cookie.HttpOnly = true;
                 options.ExpireTimeSpan = TimeSpan.FromMinutes(double.Parse(Configuration["CookieTimeSpan"]));
                 options.SlidingExpiration = true;
@@ -107,7 +107,7 @@ namespace eshopAPI
                 if (path != null && path.StartsWith("/api"))
                 {
                     var token = antiforgery.GetAndStoreTokens(context);
-                    context.Response.Cookies.Append("CSRF-TOKEN", token.RequestToken, new CookieOptions { HttpOnly = false, Domain = "eshop-qa-web.azurewebsites.net" });
+                    context.Response.Cookies.Append("CSRF-TOKEN", token.RequestToken, new CookieOptions { HttpOnly = false, Domain = ".eshop-qa-web.azurewebsites.net" });
                 }
                 return next(context);
             });
