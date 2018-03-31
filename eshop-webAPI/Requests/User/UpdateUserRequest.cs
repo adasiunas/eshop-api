2487db312f91af465230e009d8b565d25f735db2
Greta Mameniskyte <gretuka27@gmail.com>
2018-03-31 15:44:11 +0300

    remove many addresses

--- eshop-webAPI/Requests/User/UpdateUserRequest.cs
+++ eshop-webAPI/Requests/User/UpdateUserRequest.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using eshopAPI.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -10,5 +11,6 @@ namespace eshopAPI.Requests.User
         public string Name { get; set; }
         public string Surname { get; set; }
         public string Phone { get; set; }
+        public Address Address { get; set; }
     }
 }
