cf8cc35700188750ed07cf06eb4e99cdcb28c9fe
Marius Adašiūnas <marius.adasiunas@gmail.com>
2018-04-05 17:26:08 +0300

    redirectImplementation

--- eshop-webAPI/Helpers/UrlExtensions.cs
+++ eshop-webAPI/Helpers/UrlExtensions.cs
@@ -4,18 +4,16 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
 
 namespace eshopAPI.Helpers
 {
     public static class UrlExtensions
     {
-        public static string EmailConfirmationLink(this IUrlHelper urlHelper, string userId, string code, string scheme)
+        
+        public static string EmailConfirmationLink(this IUrlHelper urlHelper, string userId, string code, string domain)
         {
-            return urlHelper.Action(
-                action: nameof(AccountController.ConfirmEmail),
-                controller: "Account",
-                values: new { userId, code },
-                protocol: scheme);
+            return domain + "/confirmaccount?UserId=" + userId + "&Code=" + code; 
         }
 
         public static string ResetPasswordCallbackLink(this IUrlHelper urlHelper, string userId, string code, string scheme)
