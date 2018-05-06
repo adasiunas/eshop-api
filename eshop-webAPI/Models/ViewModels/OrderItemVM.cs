28bccc34c952e1c0ed811daa223b74f5859a5ed9
Greta Mameniskyte <gretuka27@gmail.com>
2018-05-06 18:09:03 +0300

    order history API endpoint

---
+++ eshop-webAPI/Models/ViewModels/OrderItemVM.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace eshopAPI.Models.ViewModels
+{
+    public class OrderItemVM
+    {
+        public long ID { get; set; }
+        public string SKU { get; set; }
+        public long ItemID { get; set; }
+        public string Name { get; set; }
+        public int Count { get; set; }
+        public decimal Price { get; set; }
+
+    }
+}
