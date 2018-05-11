54adcb6dc1665b465f819e6282f769f705f90667
Mantas <mantasas321@gmail.com>
2018-05-11 21:33:53 +0300

    Category action implementations

---
+++ eshop-webAPI/Requests/Categories/SubcategoryCreateRequest.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace eshopAPI.Requests.Categories
+{
+    public class SubcategoryCreateRequest
+    {
+        public int ParentID { get; set; }
+        public string Name { get; set; }
+    }
+}
