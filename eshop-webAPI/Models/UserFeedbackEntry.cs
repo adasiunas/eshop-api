b4f4ee03b25ea61a438e5f2e2f2dc24c8c92a7de
Marius Adašiūnas <marius.adasiunas@gmail.com>
2018-05-21 22:07:44 +0300

    adding entity to the dbcontext

---
+++ eshop-webAPI/Models/UserFeedbackEntry.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace eshopAPI.Models
+{
+    public class UserFeedbackEntry
+    {
+        [Key]
+        public long ID { get; set; } // Primary key
+        public string UserId { get; set; }
+        public ShopUser User { get; set; }
+        public string Message { get; set; }
+        public int Rating { get; set; }
+    }
+}
