659e1748b1a90085d9a73df990b970273fc2f8ef
Marius Adašiūnas <marius.adasiunas@gmail.com>
2018-05-22 18:33:36 +0300

    implementing feedback

---
+++ eshop-webAPI/Requests/UserFeedbackRequest.cs
@@ -0,0 +1,9 @@
+﻿namespace eshopAPI.Requests
+{
+    public class UserFeedbackRequest
+    {
+        public string UserId { get; set; }
+        public string Message { get; set; }
+        public int Rating { get; set; }
+    }
+}
