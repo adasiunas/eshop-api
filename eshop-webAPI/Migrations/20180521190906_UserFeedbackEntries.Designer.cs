5eaccaea1f7463f93d36fb49c24e8725405e52ac
Saulius Skliutas <saulius.skliutas@gmail.com>
2018-05-24 22:23:56 +0300

    Feedback migration files

--- eshop-webAPI/Migrations/20180521190906_UserFeedbackEntries.Designer.cs
+++ eshop-webAPI/Migrations/20180521190906_UserFeedbackEntries.Designer.cs
@@ -12,9 +12,10 @@ using System;
 namespace eshopAPI.Migrations
 {
     [DbContext(typeof(ShopContext))]
-    partial class ShopContextModelSnapshot : ModelSnapshot
+    [Migration("20180521190906_UserFeedbackEntries")]
+    partial class UserFeedbackEntries
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
