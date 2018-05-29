0c833ea92395efa349a76353fc500c221d8fffe7
Dovilė Makselytė <dovile.makselyte@gmail.com>
2018-05-29 16:12:14 +0300

    initial discount implementation

---
+++ eshop-webAPI/Controllers/Admin/DiscountController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using eshopAPI.DataAccess;
+using eshopAPI.Models;
+using eshopAPI.Models.ViewModels;
+using eshopAPI.Models.ViewModels.Admin;
+using Microsoft.AspNet.OData;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace eshopAPI.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/admin/discount")]
+    public class DiscountController : ODataController
+    {
+        private readonly IDiscountRepository _discountRepository;
+
+        public DiscountController(IDiscountRepository DiscountRepository)
+        {
+            _discountRepository = DiscountRepository;
+        }
+
+        [HttpGet]
+        [EnableQuery]
+        public async Task<IQueryable<AdminDiscountVM>> Get()
+        {
+            return await _discountRepository.GetAllValidDiscounts();
+        }
+    }
+}
---
+++ eshop-webAPI/Models/ViewModels/Admin/AdminDiscountVM.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace eshopAPI.Models.ViewModels.Admin
+{
+    public class AdminDiscountVM
+    {
+        public long ID { get; set; } // Primary key
+        public string Name { get; set; }
+        public int Value { get; set; }
+        public DateTime From { get; set; }
+        public DateTime To { get; set; }
+
+        public string CategoryName { get; set; }
+        public string SubCategoryName { get; set; }
+    }
+}
