496cb7f405271653744338475e27827ffe86a305
Taurintas Kudirka <taurisx@gmail.com>
2018-10-09 19:43:06 +0300

    Added DiscountRepository tests

---
+++ eshopAPI.Tests/Builders/DiscountBuilder.cs
@@ -0,0 +1,96 @@
+﻿using eshopAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace eshopAPI.Tests.Builders
+{
+    public class DiscountBuilder
+    {
+        public static int LastId { get { return _id - 1; } }
+
+        public long ID { get { return _id++; } }
+        public string Name { get; set; } = "DiscountName";
+        public int Value { get; set; } = 10;
+        public bool IsPercentages { get; set; } = true;
+        public DateTime To { get; set; } = DateTime.UtcNow.AddDays(1);
+
+        static int _id = 1;
+        Discount _discount;
+
+        public DiscountBuilder()
+        {
+            _discount = WithDefaultValues();
+        }
+
+        public DiscountBuilder New()
+        {
+            _discount = WithDefaultValues();
+            return this;
+        }
+
+        public DiscountBuilder SetName(string name)
+        {
+            _discount.Name = name;
+            return this;
+        }
+
+        public DiscountBuilder SetItem(Item item)
+        {
+            ClearItem();
+            _discount.Item = item;
+            _discount.ItemID = item.ID;
+            return this;
+        }
+
+        public DiscountBuilder SetCategory(Category item)
+        {
+            ClearItem();
+            _discount.Category = item;
+            _discount.CategoryID = item.ID;
+            return this;
+        }
+
+        public DiscountBuilder SetSubcategory(SubCategory item)
+        {
+            ClearItem();
+            _discount.SubCategory = item;
+            _discount.SubCategoryID = item.ID;
+            return this;
+        }
+
+        public DiscountBuilder SetDate(DateTime to)
+        {
+            _discount.To = to;
+            return this;
+        }
+
+        public Discount Build()
+        {
+            _discount.ID = ID;
+            return _discount;
+        }
+
+        void ClearItem()
+        {
+            _discount.Item = null;
+            _discount.ItemID = null;
+            _discount.Category = null;
+            _discount.CategoryID = null;
+            _discount.SubCategory = null;
+            _discount.SubCategoryID = null;
+        }
+
+        Discount WithDefaultValues()
+        {
+            _discount = new Discount
+            {
+                To = To,
+                Name = Name,
+                IsPercentages = IsPercentages,
+                Value = Value
+            };
+            return _discount;
+        }
+    }
+}
---
+++ eshopAPI.Tests/DataAccess/DiscountRepositoryTests/InsertDiscount.cs
@@ -0,0 +1,99 @@
+﻿using eshopAPI.DataAccess;
+using eshopAPI.Models;
+using eshopAPI.Tests.Builders;
+using eshopAPI.Tests.Helpers;
+using Microsoft.EntityFrameworkCore;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xunit;
+
+namespace eshopAPI.Tests.DataAccess.DiscountRepositoryTests
+{
+    public class InsertDiscount
+    {
+        long _firstDiscountId;
+        DiscountRepository _repository;
+        DbContextOptions<ShopContext> _options;
+
+        public InsertDiscount()
+        {
+            _options = new DbContextOptionsBuilder<ShopContext>()
+                .UseInMemoryDatabase(databaseName: "InsertDiscount")
+                .Options;
+            _repository = GetDiscountRepository();
+        }
+
+        [Fact]
+        public async void Success()
+        {
+            Discount newDiscount = new DiscountBuilder().Build();
+            Discount discount = await _repository.InsertDiscount(newDiscount);
+            await _repository.SaveChanges();
+            Discount foundDiscount = GetDiscountByID(newDiscount.ID);
+            Assert.Equal(newDiscount.Name, foundDiscount.Name);
+            Assert.Equal(newDiscount.Name, discount.Name);
+        }
+
+        [Fact]
+        public async void Failure()
+        {
+            Discount newDiscount = new DiscountBuilder().Build();
+            newDiscount.ID = _firstDiscountId;
+
+            await Assert.ThrowsAnyAsync<Exception>(async () =>
+            {
+                Discount discount = await _repository.InsertDiscount(newDiscount);
+                await _repository.SaveChanges();
+            }); 
+        }
+
+        Discount GetDiscountByID(long id)
+        {
+            Discount discount;
+            using (ShopContext context = new ShopContext(_options))
+            {
+                discount = context.Discounts.Where(o => o.ID == id).FirstOrDefault();
+            }
+            return discount;
+        }
+
+        private DiscountRepository GetDiscountRepository()
+        {
+            ShopContext dbContext = new ShopContext(_options);
+            dbContext.Database.EnsureDeleted();
+            dbContext.Database.EnsureCreated();
+            using (ShopContext context = new ShopContext(_options))
+            {
+                SeedData(context);
+            }
+            return new DiscountRepository(dbContext);
+        }
+
+        void SeedData(ShopContext context)
+        {
+            DiscountBuilder builder = new DiscountBuilder();
+            List<Discount> discounts = new List<Discount>()
+            {
+                builder.New().SetItem(new Item { ID = 1 }).Build(),
+                builder.New().SetItem(new Item { ID = 1 }).Build(),
+                builder.New().SetItem(new Item { ID = 3 }).Build(),
+                builder.New().SetItem(new Item { ID = 3 }).SetDate(DateTime.UtcNow.AddDays(-1)).Build(),
+                builder.New().SetCategory(new Category { ID = 1 }).Build(),
+                builder.New().SetCategory(new Category { ID = 3 }).Build(),
+                builder.New().SetCategory(new Category { ID = 3 }).Build(),
+                builder.New().SetCategory(new Category { ID = 4 }).Build(),
+                builder.New().SetSubcategory(new SubCategory { ID = 1 }).Build(),
+                builder.New().SetSubcategory(new SubCategory { ID = 2 }).Build(),
+                builder.New().SetSubcategory(new SubCategory { ID = 1 }).Build(),
+                builder.New().SetSubcategory(new SubCategory { ID = 1 }).Build(),
+            };
+            _firstDiscountId = discounts.First().ID;
+
+            context.Discounts.AddRange(discounts);
+            context.SaveChanges();
+        }
+    }
+}
